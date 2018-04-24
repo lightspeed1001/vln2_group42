e18ca48fdd9e4367f1ea2486d6365a8106d31d43
Mikael Sigmundsson <lightspeed1001@gmail.comwq>
2018-04-24 13:30:23 +0000

    Initial commit. Made a new mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vln2_group42.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
