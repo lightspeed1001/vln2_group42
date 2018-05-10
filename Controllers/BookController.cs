ab5984372a90510796af4895d3397bb02ef7488a
kjartanandersen <svidakjammi@gmail.com>
2018-05-10 18:46:57 +0000

    fix more shit

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -5,11 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BookCave.Models;
-<<<<<<< HEAD
-using BookCave.Services;
-=======
 using BookCave.Models.ViewModels;
->>>>>>> 1074473bbd2b88e955e14b267bfe97c064eb688d
 using BookCave.Repositories;
 
 namespace BookCave.Controllers
